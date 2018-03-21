f1da7e41a0a01ef86bf2e880a283e9cb06f76d61
xulei <xljiadahao@gmail.com>
2018-03-21 13:54:02 +0800

    SSL connection for rest client

---
+++ src/main/java/com/snippet/jwt/client/RestClientConfig.java
@@ -0,0 +1,61 @@
+package com.snippet.jwt.client;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.security.KeyStore;
+
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@PropertySource("classpath:config.properties")
+public class RestClientConfig {
+
+    @Bean
+    public RestOperations restOperations(ClientHttpRequestFactory clientHttpRequestFactory) throws Exception {
+        return new RestTemplate(clientHttpRequestFactory);
+    }
+
+    @Bean
+    public ClientHttpRequestFactory clientHttpRequestFactory(HttpClient httpClient) {
+        return new HttpComponentsClientHttpRequestFactory(httpClient);
+    }
+
+    @Bean
+    public HttpClient httpClient(@Value("${keystore.file}") String file, 
+            @Value("${keystore.pass}") String password) throws Exception {
+        KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        FileInputStream instream = new FileInputStream(new File(file));
+        try {
+            trustStore.load(instream, password.toCharArray());
+        } finally {
+            instream.close();
+        }
+        SSLContext sslcontext = SSLContexts.custom()
+                .loadTrustMaterial(trustStore, new TrustSelfSignedStrategy()).build();
+        @SuppressWarnings("deprecation")
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslcontext,
+                SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+        return HttpClients.custom().setSSLSocketFactory(sslsf).build();
+    }
+
+    @Bean
+    public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+}
